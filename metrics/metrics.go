d4cf1b7a3814d0263bf84c769aa3f23703444709
Simon Pasquier <spasquie@redhat.com>
2019-10-10 18:35:05 +0200

    Fix format

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -56,6 +56,7 @@ func NewStoreMetrics(r prometheus.Registerer) *StoreMetrics {
 		duration: duration,
 	}
 }
+
 func (m *StoreMetrics) wrapRequest(op string, f func() bool) {
 	now := time.Now()
 	m.total.WithLabelValues(op).Inc()
